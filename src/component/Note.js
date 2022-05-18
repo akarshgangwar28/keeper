06fdeefcc9f51e4e741da8b96d7b64bf08f697f0
Akarsh Gangwar <akarshprog28@gmail.com>
2022-05-18 22:44:21 +0530

    way of rendering  Note compnent changed

--- src/component/Note.js
+++ src/component/Note.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import x from "../notes"
 
-export default function Note(props) {
-return(
+export default function Note() {
+
+function extracter(notes)
+{
+  return(
     <div className='note'>
-        <h1>{props.title}</h1>
-        <p>{props.content} </p>
+        <h1>{notes.titile}</h1>
+        <p>{notes.content} </p>
    </div>
-)
+  )
+}
 
+  return (
+   
+   <>
 
+   {x.map(extracter)}
 
-  
+</>
+  )
 }
